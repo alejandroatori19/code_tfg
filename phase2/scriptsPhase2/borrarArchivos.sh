6f3e40420e4116c2f87687b4d046affeaf530abe
alejandroatori19 <alejandroatori@gmail.com>
2024-02-22 12:52:41 +0100

    Todo funcional

---
+++ phase2/scriptsPhase2/borrarArchivos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo rm /home/robocomp/dataset/colorFrames/*
+sudo rm /home/robocomp/dataset/depthFrames/*
+
+# For do it in your folder. Just use the next one changing <path> with the path of your folder
+
+# sudo rm <path>/*
+# sudo rm <path>/*
+
